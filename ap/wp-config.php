73e43eadd533faf6f93fac87c5c7447335033fc5
EC2 Default User <ec2-user@ip-172-31-48-49.us-west-2.compute.internal>
2026-05-13 06:25:15 +0000

    lv1-q3: systemctl互換スクリプト導入、wp-config.php DB_NAME誤り+セミコロン欠落問題

--- ap/wp-config.php
+++ ap/wp-config.php
@@ -1,14 +1,12 @@
 <?php
-
 // データベース設定
-define( 'DB_NAME', 'Error' )
+define( 'DB_NAME', 'wordpress-database' )
 define( 'DB_USER', 'wordpress-user' );
 define( 'DB_PASSWORD', 'wordpress-password' );
 define( 'DB_HOST', '10.10.10.13' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
-
-// 
+//
 define( 'AUTH_KEY',         'training-auth-key-0001' );
 define( 'SECURE_AUTH_KEY',  'training-secure-auth-key-0002' );
 define( 'LOGGED_IN_KEY',    'training-logged-in-key-0003' );
@@ -17,17 +15,13 @@ define( 'AUTH_SALT',        'training-auth-salt-0005' );
 define( 'SECURE_AUTH_SALT', 'training-secure-auth-salt-0006' );
 define( 'LOGGED_IN_SALT',   'training-logged-in-salt-0007' );
 define( 'NONCE_SALT',       'training-nonce-salt-0008' );
-
-// 
+//
 $table_prefix = 'wp_';
-
-// 
+//
 define( 'WP_DEBUG', false );
-
 //
 if ( ! defined( 'ABSPATH' ) ) {
     define( 'ABSPATH', __DIR__ . '/' );
 }
-
-// 
+//
 require_once ABSPATH . 'wp-settings.php';
